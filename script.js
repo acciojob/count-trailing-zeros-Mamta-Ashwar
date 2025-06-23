dc3dcd01a45c84bd6875568c0765d17e2bb8b1ad
Mamta-Ashwar <ashwarmamta22@gmail.com>
2025-06-23 09:39:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
+  let count = 0;
+  for (let i = 5; i <= n; i *= 5) {
+    count += Math.floor(n / i);
+  }
+  return count;
 }
 
-const input = prompt("Enter a number");
-alert(trailingZeros(input));
+const input = prompt("Enter a non-negative integer up to 10000:");
+const num = Number(input);
+
+if (Number.isInteger(num) && num >= 0 && num <= 10000) {
+  const zeros = trailingZeros(num);
+  alert(`Trailing zeros in ${num}! is: ${zeros}`);
+} else {
+  alert("Invalid input! Please enter a non-negative integer up to 10000.");
+}
